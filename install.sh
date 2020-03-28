9875cb8848dfe67e33a67f336d854fcfcd67a5c4
Mohd Faiz Hasim <faizhasim@gmail.com>
2020-03-29 07:40:54 +0800

    remove starship

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ chsh -s /bin/zsh
 cp ./zshrc.sh ~/.zshrc
 
 sudo mkdir -p /usr/local/share/antigen
-curl -L git.io/antigen > /usr/local/share/antigen/antigen.zsh
+curl -L git.io/antigen > /tmp/antigen.zsh
+sudo mv /tmp/antigen.zsh /usr/local/share/antigen/antigen.zsh
 chmod a+r /usr/local/share/antigen/antigen.zsh
 
-curl -fsSL https://starship.rs/install.sh | bash
--- zshrc.sh
+++ zshrc.sh
@@ -10,7 +10,26 @@ setup_plugins() {
   antigen bundle nvm
 }
 
-setup_plugins && antigen apply
+setup_theme() {
+  DEFAULT_USER=$USER
+  POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+  POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=true
+  POWERLEVEL9K_NODE_VERSION_FOREGROUND='black'
+  POWERLEVEL9K_TIME_FORMAT="%D{%H:%M:%S}"
+  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time context dir vcs)
+  POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status rbenv background_jobs)
+  POWERLEVEL9K_STATUS_VERBOSE=true
+  POWERLEVEL9K_SHOW_CHANGESET=true
+  POWERLEVEL9K_CHANGESET_HASH_LENGTH=7
+
+  POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
+  POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
+  POWERLEVEL9K_INSTALLATION_PATH=$ANTIGEN_BUNDLES/bhilburn/powerlevel9k
+
+  antigen theme bhilburn/powerlevel9k powerlevel9k
+}
+
+setup_plugins && setup_theme && antigen apply
 
 export LANG="en_US.UTF-8"
 export LC_COLLATE="en_US.UTF-8"
